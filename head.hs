de239f3cdb3022a249cf88dfa03a7c5741a957b1
Owen Rodley <Strigoides@gmail.com>
2013-03-31 19:46:08 +1300

    Initial version of `head' Doesn't handle any options

---
+++ head.hs
@@ -0,0 +1,10 @@
+import System (getArgs)
+import System.IO
+
+-- Read the first n lines of a file or stdin
+main = do
+	args <- getArgs
+	input <- if args == []
+	         	then getContents
+	         	else readFile $ head args
+	mapM_ putStrLn $ take 10 $ lines input

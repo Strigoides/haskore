242c59270cb28dd599f6c962ea139645ed3e06b7
Owen Rodley <Strigoides@gmail.com>
2013-05-11 16:08:29 +1200

    seq

---
+++ seq.hs
@@ -0,0 +1,14 @@
+import System.Environment
+import System.Exit
+
+main = do
+	args <- getArgs
+	let len = length $ args in
+		if len > 2 || len == 0
+			then do
+				putStrLn "Wrong number of args"
+				exitWith $ ExitFailure 1
+			else mapM_ print $
+				if len == 1
+					then [1.. read $ head args]
+					else [read $ head args .. read $ args !! 1]

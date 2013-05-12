7ea40aaa4a2457e6f5910843247282cfcf847b9e
Owen Rodley <Strigoides@gmail.com>
2013-05-12 15:30:48 +1200

    Initial version of factor Doesn't handle reading from stdin

---
+++ factor.hs
@@ -0,0 +1,18 @@
+import Data.List
+import System.Environment
+
+primeFactors :: Int -> [Int]
+primeFactors 1 = []
+primeFactors n = m : primeFactors (n `quot` m)
+	where m = head $ dropWhile ((/= 0) . (n `mod`)) [2..n]
+
+printFactors :: Int -> IO ()
+printFactors n = do
+	putStr   $ show n ++ ": "
+	putStrLn $ intercalate " " $ map show $ primeFactors n
+
+main = do
+	args <- getArgs
+	if null args
+		then putStrLn "Invalid number of arguments"
+		else mapM_ (printFactors . read) args

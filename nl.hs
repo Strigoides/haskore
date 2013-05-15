d7d8931883156c5610471d276611f6353d7ca8e3
Owen Rodley <Strigoides@gmail.com>
2013-05-16 00:30:58 +1200

    Remove parens I should really run hlint *before* commiting...

--- nl.hs
+++ nl.hs
@@ -1,4 +1,4 @@
 import Text.Printf
 
 main = getContents >>=
-    mapM_ putStrLn . (zipWith (printf "%6d  %s") [(1::Int)..] . lines)
+    mapM_ putStrLn . zipWith (printf "%6d  %s") [(1::Int)..] . lines
